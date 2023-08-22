1a0bda0c5e16fc5d6c5d3c1ddeab00ea74d9bfb6
justindstein <jaydstein@gmail.com>
2023-08-22 14:46:36 -0300

    Agular drag and torque fine-tuning.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     private float inputHorizontal;
     private bool inputJump;
 
+    //[SerializeField] private float rotateVelocity; // -2
+    [SerializeField] private float torqueForce; // -5
+    [SerializeField] private float jumpForce; // 7.5
+
     void Start()
     {
         this.rigidBody = this.GetComponent<Rigidbody2D>();
@@ -25,14 +29,15 @@ public class PlayerController : MonoBehaviour
         Debug.Log(string.Format("[Horizontal: {0}] [Vertical: {1}] [isJump {2}] ", this.inputHorizontal, this.inputVertical, this.inputJump));
 
         // Player rotation
-        this.transform.Rotate(new Vector3(0, 0, this.inputHorizontal * -2f), Space.Self);
+        //this.transform.Rotate(new Vector3(0, 0, (this.inputHorizontal * this.rotateVelocity)), Space.Self);
+        this.rigidBody.AddTorque(this.inputHorizontal * this.torqueForce);
 
         // TODO: Jump only if player is on ground
         // Player jump
         if (this.inputJump)
         {
             Debug.Log("Jumping");
-            this.rigidBody.velocity = new Vector2(this.rigidBody.velocity.x, this.rigidBody.velocity.y + 7.5f);
+            this.rigidBody.velocity = new Vector2(this.rigidBody.velocity.x, (Vector2.up.y * this.jumpForce));
             this.inputJump = false;
         }
     }
